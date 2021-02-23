26bb6536d7fd549c283dc1fe74cbdfaea6b8787d
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2021-02-23 15:59:44 -0500

    added tracebacks for snapshots

--- snapshot.py
+++ snapshot.py
@@ -1,29 +1,44 @@
 import os
 import signal
+import traceback
+
+SNAPSHOTS = []
 
 
-LAYERS = 0
+def is_in_snapshot():
+    return len(SNAPSHOTS) > 0
 
 
-def in_snapshot():
-    return LAYERS > 0
-
-
-def only_outside_snapshot(f):
+def not_in_snapshot(f):
     def __inner(*args, **kwargs):
-        if not LAYERS:
+        if not SNAPSHOTS:
             return f(*args, **kwargs)
     return __inner
 
 
-def snapshot():
-    global LAYERS
+def format_snapshot_list(context_frames=3):
+    result = ''
+    for i, (pid, bt) in enumerate(SNAPSHOTS):
+        result += '=' * 20 + f' Snapshot #{i} ' + '=' * 20 + '\n'
+        result += '\n'.join(bt[-context_frames:])
+    return result
+
+
+def print_snapshot_list(context_frames=3):
+    print(format_snapshot_list(context_frames=context_frames))
+
+
+def snapshot(context=None):
+    global SNAPSHOTS
     child_pid = os.fork()
+
+    context = context if context is not None else traceback.format_stack()
 
     if child_pid == 0:
         # in the child we act like nothing happened
-        print(f"SNAPSHOT process pid={os.getpid()} created, continuing in snapshot.")
-        LAYERS += 1
+        my_pid = os.getpid()
+        print(f"SNAPSHOT process pid={my_pid} created, continuing in snapshot.")
+        SNAPSHOTS.append((my_pid, context))
         return True
 
     def sig_to_child(signum, frame):
@@ -47,6 +62,8 @@ def snapshot():
 
     # we're done handling it, reset the SIGINT handler
     signal.signal(signal.SIGINT, old_handler)
+    print(f"Reverted to snapshot #{len(SNAPSHOTS)} with pid={os.getpid()}!")
+    print('\n'.join(context[-3:]))
     return False
 
 
