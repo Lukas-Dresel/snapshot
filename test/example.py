9812e2406dcf904dc4e92c14e2f9511c9b2fe52e
Lukas Dresel <Lukas-Dresel@users.noreply.github.com>
2021-05-10 14:27:07 -0400

    write string please

--- test/example.py
+++ test/example.py
@@ -1,8 +1,9 @@
 import time
+import ipdb
 
 def f():
     print("Trust me, you really want to step into me!!!")
-    with open("/tmp/secret", 'wb') as f:
+    with open("/tmp/secret", 'w') as f:
         f.write('muhahaha, you have discovered my secret lair!')
     return
 
@@ -11,6 +12,7 @@ for i in reversed(range(TIME)):
     time.sleep(i)
     print(f"Have to wait {i:2d} seconds longer, hope you don't mess up debugging me after :P")
 
+ipdb.set_trace()
 f()
 print("I hope you saw the secrets inside f()! No going back now.")
 
